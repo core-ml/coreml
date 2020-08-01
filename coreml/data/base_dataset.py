9b984f116757425f4f5214f6a8caf3d1f2084d76
Aman Dalmia <amandalmia18@gmail.com>
2020-08-01 13:17:12 +0000

    NEW: base dataset class

---
+++ coreml/data/base_dataset.py
@@ -0,0 +1,45 @@
+"""Defines base dataset class for loading audio datasets."""
+import random
+from abc import abstractmethod
+from typing import List
+
+from torch.utils.data import Dataset
+from coreml.utils.typing import DatasetConfigDict
+
+random.seed(0)
+
+
+class BaseDataset(Dataset):
+    """
+    Defines the base dataset object that needs to be inherited
+    by any task-specific dataset class and by other base classes
+    for different modalities.
+
+    :param dataset_config: defines the config for the data to be loaded.
+        The config is specified by a list of dicts, with each dict representing
+        (dataset_name, dataset_version, mode [train, test, val])
+    :type dataset_config: DatasetConfigDict
+    :param fraction: fraction of the data to load, defaults to 1.0
+    :type fraction: float
+    """
+    def __init__(self, dataset_config: List[DatasetConfigDict],
+                 fraction: float = 1.0):
+        self.dataset_config = dataset_config
+        self.load_items()
+        self.load_fraction(fraction)
+
+    @abstractmethod
+    def load_items(self):
+        """Load all data items as specified by self.dataset_config"""
+        pass
+
+    def load_fraction(self, fraction: float):
+        if not hasattr(self, 'items'):
+            raise ValueError('data items have not been loaded to self.items')
+
+        if fraction < 1:
+            orig_num = len(self.items)
+            final_num = int(orig_num * fraction)
+            random.shuffle(self.items)
+
+            self.items = self.items[:final_num]
