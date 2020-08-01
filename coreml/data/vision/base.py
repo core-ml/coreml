d8a4029d694d9bc0f70b6f0de7163360c470f2e9
Aman Dalmia <amandalmia18@gmail.com>
2020-08-01 15:36:34 +0000

    Adds BaseImageDataset with tests

---
+++ coreml/data/vision/base.py
@@ -0,0 +1,39 @@
+"""Defines base dataset class for loading audio datasets."""
+import random
+from abc import abstractmethod
+from typing import List
+
+from tqdm import tqdm
+from torch.utils.data import Dataset
+from coreml.utils.typing import DatasetConfigDict
+from coreml.data.vision.image import Image
+from coreml.data.base_dataset import BaseDataset
+from coreml.data.utils import read_dataset_from_config
+
+random.seed(0)
+
+
+class BaseImageDataset(BaseDataset):
+    """
+    Defines the base image dataset object that needs to be inherited
+    by any task-specific dataset class for images.
+
+    :param dataset_config: defines the config for the data to be loaded. The config is
+        specified by a list of dicts, with each dict representing
+        (dataset_name, dataset_version, mode [train, test, val])
+    :type dataset_config: DatasetConfigDict
+    :param fraction: fraction of the data to load, defaults to 1.0
+    :type fraction: float
+    """
+    def load_items(self):
+        """Load all image data items as specified by self.dataset_config"""
+        self.items = []
+
+        for dataset_config in self.dataset_config:
+            data_info = read_dataset_from_config(dataset_config)
+
+            for i in tqdm(range(len(data_info['file'])), desc='Loading items'):
+                path, label = data_info['file'][i], data_info['label'][i]
+
+                image_item = Image(path=path, label=label)
+                self.items.append(image_item)
---
+++ tests/data/vision/test_base.py
@@ -0,0 +1,35 @@
+"""Tests coreml.data.vision.base"""
+import unittest
+import subprocess
+from os.path import join, exists
+import torch
+import cv2
+import numpy as np
+from coreml.config import DATA_ROOT
+from coreml.data.vision.base import BaseImageDataset
+
+
+class BaseImageDatasetTestCase(unittest.TestCase):
+    """Class to run tests on BaseImageDataset"""
+    @classmethod
+    def setUpClass(cls):
+        if not exists(join(DATA_ROOT, 'CIFAR10')):
+            subprocess.call(
+                'python /workspace/coreml/tasks/data/classification/CIFAR10.py',
+                shell=True)
+
+    def test_image_classification_dataset(self):
+        """Test default functionality of Image.read()"""
+        dataset_config = [
+            {
+                'name': 'CIFAR10',
+                'version': 'default',
+                'mode': 'test'
+            }
+        ]
+        dataset = BaseImageDataset(dataset_config)
+        self.assertEqual(len(dataset.items), 10000)
+
+
+if __name__ == "__main__":
+    unittest.main()
