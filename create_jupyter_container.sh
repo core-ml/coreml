77315b79907d863fd3b81cafed63ed3bfe18493e
Aman Dalmia <amandalmia18@gmail.com>
2020-08-01 14:08:07 +0000

    Add container for creating jupyter lab

---
+++ create_jupyter_container.sh
@@ -0,0 +1,28 @@
+gpu=0
+port=8888
+while getopts "g:n:p:" OPTION; do
+    case $OPTION in
+        g) gpu=$OPTARG;;
+		n) name=$OPTARG;;
+		p) port=$OPTARG;;
+        *) exit 1 ;;
+    esac
+done
+
+image=adalmia/coreml:v1.0
+
+if [[ -z $WANDB_API_KEY ]] ; then
+	echo "ERROR: set the environment variable WANDB_API_KEY"
+	exit 0
+fi
+
+NV_GPU=$gpu nvidia-docker run --rm -it \
+	--name "$gpu"_"$name" \
+	-p $port:$port \
+	-v /path/to/coreml/:/workspace/coreml \
+	-v /path/to/outputs/$user:/output \
+	-v /path/to/data:/data \
+	--env WANDB_DOCKER=$image \
+	--env WANDB_API_KEY=$WANDB_API_KEY \
+	--ipc host \
+	$image bash
