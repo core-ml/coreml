b7a4aa35cb318635cb7505f7c5bbc63ce201cfd8
Aman Dalmia <amandalmia18@gmail.com>
2020-08-01 10:37:44 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coreml",
+    version="0.0.1",
+    author="Aman Dalmia",
+    author_email="amandalmia18@gmail.com",
+    description="Generic Framework for ML projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dalmia/coreml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
