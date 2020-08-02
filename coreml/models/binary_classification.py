841d981f17d9756a5e7e51195d4d2b351145cd57
Aman Dalmia <amandalmia18@gmail.com>
2020-08-02 15:40:21 +0000

    Added evaluate function in binary_classification.py

--- coreml/models/binary_classification.py
+++ coreml/models/binary_classification.py
@@ -560,4 +560,27 @@ class BinaryClassificationModel(Model):
         :param log_summary: whether to log epoch summary
         :type log_summary: bool, defaults to True
         """
-        pass
+        ckpt_name = basename(self.load_path).split('.')[0]
+        cache_path = join(
+            self.config.output_dir, 'evaluation', ckpt_name, 'cache',
+            '{}.pt'.format(mode))
+        makedirs(dirname(cache_path), exist_ok=True)
+
+        # load cache if `ignore_cache=False`
+        if exists(cache_path) and not ignore_cache:
+            logging.info(
+                color('Using cached values from {}'.format(cache_path), 'red'))
+            results = torch.load(cache_path)
+        else:
+            logging.info(color('Ignoring cache', 'red'))
+            # prevent logging anything into wandb when processing epoch
+            results = self.process_epoch(
+                data_loader, mode=mode, training=False, use_wandb=False,
+                log_summary=log_summary)
+
+            logging.info(
+                color('Saving cached values to {}'.format(cache_path), 'red'))
+            torch.save(results, cache_path)
+
+        if data_only:
+            return results
---
+++ tests/models/test_clf_evaluation.py
@@ -0,0 +1,57 @@
+"""Tests cac.models.classification.ClassificationModel evaluation"""
+import os
+from os.path import dirname, join, exists
+from copy import deepcopy
+import torch
+import wandb
+import unittest
+from tqdm import tqdm
+import numpy as np
+from coreml.config import Config
+from coreml.utils.logger import set_logger, color
+from coreml.data.dataloader import get_dataloader
+from coreml.models.binary_classification import BinaryClassificationModel
+
+
+class BinaryClassificationModelEvaluationTestCase(unittest.TestCase):
+    """Class to check the evaluation of BinaryClassificationModel"""
+    @classmethod
+    def setUpClass(cls):
+        version = 'configs/defaults/binary-cifar-classification.yml'
+        cls.cfg = Config(version)
+        cls.cfg.data['dataset']['params'] = {
+            'val': {
+                'fraction': 0.1
+            }
+        }
+        cls.cfg.num_workers = 10
+
+    def test_1_model_fitting(self):
+        """Test model.fit()"""
+        set_logger(join(self.cfg.log_dir, 'train.log'))
+
+        tester_cfg = deepcopy(self.cfg)
+        tester_cfg.model['epochs'] = 1
+        classifier = BinaryClassificationModel(tester_cfg)
+        classifier.fit(debug=True, use_wandb=False)
+
+    def test_2_evaluate(self):
+        """Test model.evaluate()"""
+        set_logger(join(self.cfg.log_dir, 'train.log'))
+
+        tester_cfg = deepcopy(self.cfg)
+        tester_cfg.model['load']['version'] = 'default'
+        tester_cfg.model['load']['load_best'] = True
+        kwargs = {'threshold': 0.5}
+        model = BinaryClassificationModel(tester_cfg)
+        dataloader, _ = get_dataloader(
+            tester_cfg.data, 'val',
+            tester_cfg.model['batch_size'],
+            num_workers=4,
+            shuffle=False,
+            drop_last=False)
+        model.evaluate(dataloader, 'val', False, **kwargs)
+
+
+if __name__ == "__main__":
+    unittest.main()
