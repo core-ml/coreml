73772803cf8c0862394104ca995cfb47ccaee8c3
Aman Dalmia <amandalmia18@gmail.com>
2020-08-02 19:15:30 +0000

    Added support for balanced sampler

--- coreml/data/sampler.py
+++ coreml/data/sampler.py
@@ -47,5 +47,85 @@ class DataSampler(Sampler):
         return self.len
 
 
+class ClassificationDataSampler(DataSampler):
+    """Custom sampler to decide the ordering of samples for classification
+
+    :param dataset: the dataset object from which to sample
+    :type dataset: :class:`~torch.utils.data.Dataset`
+    :param shuffle: decides the functionality for the sampler,
+        defaults to True
+    :type shuffle: bool, optional
+    :param seed: random seed to use for sampling, defaults to 0
+    :type seed: int, optional
+    :param target_transform: defines the transformation to be applied on the
+        raw targets to make them processable; if label_index is provided,
+        target_transform.transforms[label_index] is used instead; defaults to
+        None
+    :type target_transform: Any
+    :param mode: mode of sampling; choices are [`default`, `balanced`]; for
+        `default`, it matches the default sampling behaviour. For `balanced`,
+        it ensures class balance per batch and drops the examples; defaults
+        to `default`
+    :type mode: str, optional
+    """
+    def __init__(self, dataset: Dataset, shuffle: bool = True, seed: int = 0,
+                 target_transform: Any = None, mode: str = 'default'):
+        super(ClassificationDataSampler, self).__init__(dataset, shuffle, seed)
+        self._check_params(dataset, shuffle, target_transform, mode)
+        self.mode = mode
+
+        if mode == 'balanced':
+            self.target_transform = target_transform
+            labels = [item.label['classification'] for item in dataset.items]
+            self.labels = np.array([target_transform(
+                label) for label in labels])
+
+            _, indices = np.unique(self.labels, return_inverse=True)
+
+            # tracks the list of indices corresponding to each label
+            self.label_indices_map = defaultdict(list)
+
+            for index, class_index in enumerate(indices):
+                self.label_indices_map[class_index].append(index)
+
+            # tracks the minimum number of examples across classes
+            self.min_count = min(
+                [len(indices) for _, indices in self.label_indices_map.items()])
+            self.load_fn = self.load_balanced
+
+            # length = number of classes * min_count
+            self.len = self.min_count * len(self.label_indices_map)
+
+    def load_balanced(self):
+        """
+        Returns a list of indices with class balance per batch.
+        It returns K * C indices where C is the number of classes and K
+        is the minimum number of examples across classes.
+        """
+        if self.shuffle:
+            for key in self.label_indices_map:
+                random.shuffle(self.label_indices_map[key])
+
+        indices = []
+
+        for i in range(self.min_count):
+            # need to use `sorted` here to ensure that the ordering of keys is
+            # not affected by which key was created first
+            indices.extend([subindices[i] for _, subindices in sorted(
+                self.label_indices_map.items())])
+
+        return indices
+
+    @staticmethod
+    def _check_params(dataset, shuffle, target_transform, mode):
+        assert mode in ['default', 'balanced']
+        if mode == 'default':
+            return
+
+        assert isinstance(dataset.items[0].label, dict)
+        assert hasattr(target_transform, 'classes')
+
+
 sampler_factory = Factory()
 sampler_factory.register_builder('default', DataSampler)
+sampler_factory.register_builder('classification', ClassificationDataSampler)
--- tests/data/test_sampler.py
+++ tests/data/test_sampler.py
@@ -3,7 +3,9 @@
 import unittest
 from torch.utils.data import Dataset
 from numpy.testing import assert_array_equal
-from coreml.data.sampler import DataSampler
+from coreml.data.vision.image import Image
+from coreml.data.transforms import ClassificationAnnotationTransform
+from coreml.data.sampler import DataSampler, ClassificationDataSampler
 
 
 class DummyDataset(Dataset):
@@ -16,6 +18,29 @@ class DummyDataset(Dataset):
 
     def __len__(self):
         return self.len
+
+
+class DummyClassificationDataset(Dataset):
+    """Defines a dummy classification dataset for testing"""
+    def __init__(self, mode='binary-1'):
+        if mode == 'binary-1':
+            # first element is label 'b'
+            self.labels = [['b'], ['a'], ['a'], ['b'], ['b']]
+        elif mode == 'binary-2':
+            # first element is label 'a'
+            self.labels = [['a'], ['a'], ['a'], ['b'], ['b']]
+        else:
+            # multi-class
+            self.labels = [['c'], ['a'], ['b'], ['a'], ['c']]
+
+        self.items = [Image(
+            '', {'classification': label}) for label in self.labels]
+
+    def __getitem__(self, index):
+        return index
+
+    def __len__(self):
+        return len(self.items)
 
 
 class DataSamplerTestCase(unittest.TestCase):
@@ -44,6 +69,121 @@ class DataSamplerTestCase(unittest.TestCase):
         # with shuffle=True, the ordering should be different from the
         # default ordering
         self.assertNotEqual(indices, true_indices)
+
+
+class ClassificationDataSamplerTestCase(unittest.TestCase):
+    """Class to run tests on ClassificationDataSampler"""
+    @classmethod
+    def setUpClass(cls):
+        cls.binary_1_dataset = DummyClassificationDataset('binary-1')
+        cls.binary_2_dataset = DummyClassificationDataset('binary-2')
+        cls.binary_target_transform = ClassificationAnnotationTransform(
+            ['b', 'a'])
+        cls.multi_dataset = DummyClassificationDataset('multi')
+        cls.multi_target_transform = ClassificationAnnotationTransform(
+            ['b', 'a', 'c'])
+
+    def test_default_no_shuffle(self):
+        """Test sampling without shuffling on mode='default'"""
+        sampler = ClassificationDataSampler(
+            self.binary_1_dataset, shuffle=False,
+            target_transform=self.binary_target_transform, mode='default')
+        indices = list(sampler)
+        true_indices = [0, 1, 2, 3, 4]
+
+        # without shuffle=True, the ordering should be the default ordering
+        self.assertEqual(indices, true_indices)
+
+    def test_default_with_shuffle(self):
+        """Test sampling with shuffling on mode='default'"""
+        sampler = ClassificationDataSampler(
+            self.binary_1_dataset, shuffle=True,
+            target_transform=self.binary_target_transform,
+            mode='default')
+        indices = list(sampler)
+        true_indices = [0, 1, 2, 3, 4]
+
+        # with shuffle=True, the ordering should be different from the
+        # default ordering
+        self.assertNotEqual(indices, true_indices)
+
+    def test_multi_balanced_no_shuffle(self):
+        """Test sampling on mode='balanced' for multiclass no shuffle"""
+        sampler = ClassificationDataSampler(
+            self.multi_dataset, shuffle=False,
+            target_transform=self.multi_target_transform,
+            mode='balanced')
+        indices = list(sampler)
+        true_indices = [2, 1, 0]
+
+        # with shuffle=False, the first occurence of each
+        # class should be returned
+        self.assertEqual(indices, true_indices)
+
+    def test_multi_balanced_with_shuffle(self):
+        """Test sampling on mode='balanced' for multiclass with shuffle"""
+        sampler = ClassificationDataSampler(
+            self.multi_dataset, shuffle=True,
+            target_transform=self.multi_target_transform,
+            mode='balanced')
+        indices = list(sampler)
+        for i, index in enumerate(indices):
+            if not i % 3:
+                target_label = 0
+            elif i % 3 == 1:
+                target_label = 1
+            else:
+                target_label = 2
+
+            self.assertEqual(sampler.labels[index], target_label)
+
+    def test_binary_balanced_no_shuffle(self):
+        """Test sampling without shuffling on mode='balanced' for binary"""
+        sampler = ClassificationDataSampler(
+            self.binary_1_dataset, shuffle=False,
+            target_transform=self.binary_target_transform,
+            mode='balanced')
+        indices = list(sampler)
+        assert_array_equal(indices, [0, 1, 3, 2])
+
+    def test_binary_1_balanced_shuffle(self):
+        """Test sampling with shuffling on mode='balanced' for binary-1"""
+        sampler = ClassificationDataSampler(
+            self.binary_1_dataset, shuffle=True,
+            target_transform=self.binary_target_transform,
+            mode='balanced')
+        indices = list(sampler)
+        for i, index in enumerate(indices):
+            if not i % 2:
+                target_label = 0
+            else:
+                target_label = 1
+
+            self.assertEqual(sampler.labels[index], target_label)
+
+    def test_binary_balanced_length(self):
+        """Test sampler length with mode='balanced' for binary-1"""
+        sampler = ClassificationDataSampler(
+            self.binary_1_dataset, shuffle=True,
+            target_transform=self.binary_target_transform,
+            mode='balanced')
+        self.assertEqual(len(sampler), 4)
+
+    def test_binary_2_balanced_shuffle(self):
+        """Test sampling with shuffling on mode='balanced' for binary-2"""
+        sampler = ClassificationDataSampler(
+            self.binary_2_dataset, shuffle=True,
+            target_transform=self.binary_target_transform,
+            mode='balanced')
+        indices = list(sampler)
+
+        for i, index in enumerate(indices):
+            if not i % 2:
+                target_label = 0
+            else:
+                target_label = 1
+
+            self.assertEqual(sampler.labels[index], target_label)
 
 
 if __name__ == "__main__":
