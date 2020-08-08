71ce0c42ba3909e9f45e6b6f6c793eb523238bca
Aman Dalmia <amandalmia18@gmail.com>
2020-08-08 09:25:48 +0000

    ENH: EfficientNet has functionality to extract features only

--- coreml/networks/backbones/efficientnet.py
+++ coreml/networks/backbones/efficientnet.py
@@ -1,18 +1,30 @@
-from efficientnet_pytorch import EfficientNet
+import torch
+from efficientnet_pytorch import EfficientNet as _EfficientNet
 
 
-def _efficientnet(name, num_classes, in_channels):
-    return EfficientNet.from_pretrained(
-        name, num_classes=num_classes, in_channels=in_channels)
+class EfficientNet(torch.nn.Module):
+    """EfficientNet with all its variants
 
+    :param variant: specific architecture to use (b0-b7)
+    :param variant: str
+    :param num_classes: number of classes to be predicted
+    :param num_classes: int
+    :param in_channles: number of input channels, defaults to 3
+    :param in_channles: int, optional
+    :param return_features: whether to only return features during inference,
+        defaults to False
+    :param return_features: bool, optional
+    """
+    def __init__(
+            self, variant: str, num_classes: int, in_channels: int = 3,
+            return_features: bool = False):
+        super(EfficientNet, self).__init__()
+        self.net = _EfficientNet.from_pretrained(
+            variant, num_classes=num_classes, in_channels=in_channels)
+        self.return_features = return_features
 
-def efficientnet_b4(num_classes, in_channels):
-    return _efficientnet('efficientnet-b4', num_classes, in_channels)
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        if self.return_features:
+            return self.net.extract_features(input)
 
-
-def efficientnet_b0(num_classes, in_channels):
-    return _efficientnet('efficientnet-b0', num_classes, in_channels)
-
-
-def efficientnet_b7(num_classes, in_channels):
-    return _efficientnet('efficientnet-b7', num_classes, in_channels)
+        return self.net(input)
--- tests/networks/backbones/test_efficientnet.py
+++ tests/networks/backbones/test_efficientnet.py
@@ -2,32 +2,46 @@
 import torch
 import torch.nn as nn
 import unittest
-from coreml.networks.backbones.efficientnet import efficientnet_b4, \
-    efficientnet_b0, efficientnet_b7
+from coreml.networks.backbones.efficientnet import EfficientNet
 
 
 class EfficientNetTestCase(unittest.TestCase):
     """Class to check the EfficientNet backbone"""
-    def test_efficientnet_b4(self):
-        """Test efficientnet_b4"""
-        net = efficientnet_b4(num_classes=2, in_channels=1)
+    def test_efficientnet_b0(self):
+        """Test efficientnet_b0"""
+        net = EfficientNet('efficientnet-b0', num_classes=2, in_channels=1)
         dummy = torch.ones((128, 1, 96, 64))
         out = net(dummy)
         self.assertTrue(out.shape, (128, 2))
 
-    def test_efficientnet_b0(self):
-        """Test efficientnet_b0"""
-        net = efficientnet_b0(num_classes=2, in_channels=1)
+    def test_efficientnet_b4(self):
+        """Test efficientnet_b4"""
+        net = EfficientNet('efficientnet-b4', num_classes=2, in_channels=1)
+        dummy = torch.ones((128, 1, 96, 64))
+        out = net(dummy)
+        self.assertTrue(out.shape, (128, 2))
+
+    def test_efficientnet_b5(self):
+        """Test efficientnet_b5"""
+        net = EfficientNet('efficientnet-b5', num_classes=2, in_channels=1)
         dummy = torch.ones((128, 1, 96, 64))
         out = net(dummy)
         self.assertTrue(out.shape, (128, 2))
 
     def test_efficientnet_b7(self):
         """Test efficientnet_b7"""
-        net = efficientnet_b7(num_classes=2, in_channels=1)
+        net = EfficientNet('efficientnet-b7', num_classes=2, in_channels=1)
         dummy = torch.ones((128, 1, 96, 64))
         out = net(dummy)
         self.assertTrue(out.shape, (128, 2))
+
+    def test_efficientnet_features(self):
+        """Test efficientnet extract_features"""
+        net = EfficientNet(
+            'efficientnet-b0', num_classes=2, in_channels=1, return_features=True)
+        dummy = torch.ones((128, 1, 224, 224))
+        out = net(dummy)
+        self.assertTrue(out.shape, (128, 1280, 7, 7))
 
 
 if __name__ == "__main__":
